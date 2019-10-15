58ba5108bc94344aa0ee83256fa0be0f795e7261
Daniel Leiton <dleitonrivera@gmail.com>
2019-10-14 22:44:27 -0600

    Changed site title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Tuanify Hosting Xperience`,
+    description: `Welcome to the best hosting option in San José, Costa Rica`,
+    author: `@DannyLeiton`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
